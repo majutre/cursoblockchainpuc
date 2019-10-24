ac49b02ee0b7c68fbdd22798d5bf64435b07cfab
Maju <56054774+majutre@users.noreply.github.com>
2019-10-24 19:19:37 -0300

    Update ERCTokenExplicado.sol

--- ERCTokenExplicado.sol
+++ ERCTokenExplicado.sol
@@ -46,54 +46,6 @@ contract Mortal is Owned  {
     function kill() public {
         if (msg.sender == contractOwner) {
             selfdestruct(contractOwner);
-        }pragma solidity 0.5.11;
-
-// Token é tipo uma rifa, uma representação de um valor.
-// O código abaixo é um padrão. Todos os contratos que seguem esse padrão são chamados de ERC20.
-//Interface: funções e campos que devem necessariamente ser aplicadas no contrato.
-
-/* LER:
-https://eips.ethereum.org/EIPS/eip-20
-https://guiadobitcoin.com.br/o-que-sao-tokens-erc-20/
-
-*/
-
-contract ERC20Interface {
-    function totalSupply() public view returns(uint amount);
-    function balanceOf(address tokenOwner) public view returns(uint balance);
-    function allowance(address tokenOwner, address spender) public view returns(uint balanceRemaining);
-    // ^retorna o valor que o spender tem direito a gastar (os X tokens).
-    function transfer(address to, uint tokens) public returns(bool status);
-    function approve(address spender, uint limit) public returns(bool status);
-  //  ^ o approve é a autorização dada ao spender para que ele possa transferir em meu nome X tokens.
-    function transferFrom(address from, address to, uint amount) public returns(bool status);
- //   ^ delega a venda de X Tokens a alguém. to=para quem vai; from=seria o dono do contrato.
-    function name() public view returns(string memory tokenName);
-    function symbol() public view returns(string memory tokenSymbol);
-
-    event Transfer(address from, address to, uint amount);
-    //^emitido quando há uma transferência. Quem mandou para aonde e quanto.
-    event Approval(address tokenOwner, address spender, uint amount);
-    //^o sender aprova ao spender X tokens.
-}
-
-contract Owned {
-    address payable contractOwner;
-
-    constructor() public { 
-        contractOwner = msg.sender; 
-    }
-    
-    function whoIsTheOwner() public view returns(address) {
-        return contractOwner;
-    }
-}
-
-
-contract Mortal is Owned  {
-    function kill() public {
-        if (msg.sender == contractOwner) {
-            selfdestruct(contractOwner);
         }
     }
 }
